546548da899a5ab4ed3be5729889785dd91967a4
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-06 15:09:04 +0300

    use of state

---
+++ state/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [value, setValue] = useState(0);
+  const handleClick = () => {
+    setValue(value + 1);
+  }
+  return <div className="App">
+    <button onClick={handleClick}>Add Course</button>
+    <div>Course Count: {value}</div>
+  </div>;
+}
+
+export default App;
