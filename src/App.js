303d572a809070da39bac0c1eb8a62efd11e0603
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-06 12:50:50 +0300

    jsx logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const nameSurname = "Ehlullah Karakurt"
+  const students = 43000;
+  const isTrue = false;
+  const date = 'date';
+  const url = 'https://picsum.photos/250/250';
   return (
-    <div className="App">
-      <header className="App-header">Ehlullah
-      </header>
+    // <div>
+    //   <h1>İsim Soyisim : {nameSurname}</h1>
+    //   <p>Öğrenci sayısı : {students}</p>
+    //   <p>Doğru mu : {isTrue}</p>
+    //   <input type="text" />
+
+    //   {isTrue ? <p>Öğrenci sayısı</p> : <p>Kurs sayısı </p>}
+
+    //   <input type={date} />
+    //   <img src={url} alt="" />
+    // </div>
+
+    <div style={{
+      width:'250px',
+      height:'250px',
+      backgroundColor:'blue',
+      fontSize:'30px'
+    }}>
+
     </div>
   );
 }
