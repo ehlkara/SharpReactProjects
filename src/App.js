994b1c0adfc3085e90c167e16f1f4c14397ad736
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-06 13:56:22 +0300

    edited css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import "./App.css";
 import Course from "./Course";
 import Angular from "./images/angular.jpg";
@@ -8,28 +9,45 @@ import kompleweb from "./images/kompleweb.jpg";
 function App() {
   return (
     <div className="App">
-      {/* <img src={Angular} /> */}
-
-      <Course
-        image={Angular}
-        title="Angular"
-        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
-      />
-      <Course
-        image={bootstrap5}
-        title="Bootstrap 5"
-        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
-      />
-      <Course
-        image={ccsharp}
-        title="Komple Web"
-        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
-      />
-      <Course
-        image={kompleweb}
-        title="Frontend"
-        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
-      />
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">My Courses</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Angular}
+              title="Angular"
+              description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={bootstrap5}
+              title="Bootstrap 5"
+              description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={ccsharp}
+              title="Komple Web"
+              description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={kompleweb}
+              title="Frontend"
+              description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+            />
+          </div>
+        </div>
+        </section>
+      </div>
     </div>
   );
 }
