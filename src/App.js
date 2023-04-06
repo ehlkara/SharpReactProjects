143e7c9bd99263a1b451f88f919e7cb9d0a7bd34
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-06 13:05:41 +0300

    rerender component logic

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import "./App.css";
+import  Course from "./Course";
 
 function App() {
-  const nameSurname = "Ehlullah Karakurt"
-  const students = 43000;
-  const isTrue = false;
-  const date = 'date';
-  const url = 'https://picsum.photos/250/250';
   return (
-    // <div>
-    //   <h1>İsim Soyisim : {nameSurname}</h1>
-    //   <p>Öğrenci sayısı : {students}</p>
-    //   <p>Doğru mu : {isTrue}</p>
-    //   <input type="text" />
 
-    //   {isTrue ? <p>Öğrenci sayısı</p> : <p>Kurs sayısı </p>}
-
-    //   <input type={date} />
-    //   <img src={url} alt="" />
-    // </div>
-
-    <div style={{
-      width:'250px',
-      height:'250px',
-      backgroundColor:'blue',
-      fontSize:'30px'
-    }}>
-
+    <div className="App">
+      <Course/>
+      <Course/>
+      <Course/>
+      <Course/>
     </div>
   );
 }
---
+++ src/Course.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// export const Course = () => {
+//   return <div>Course</div>;
+// };
+
+function Course() {
+  return <div>Course</div>;
+}
+
+export default Course;
