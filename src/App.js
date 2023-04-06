e3a413320f8805d02a94f06521ab70182332eedb
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-06 13:21:19 +0300

    use of props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
-import  Course from "./Course";
+import Course from "./Course";
 
 function App() {
   return (
-
     <div className="App">
-      <Course/>
-      <Course/>
-      <Course/>
-      <Course/>
+      <Course
+        title="Angular"
+        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+      />
+      <Course
+        title="Bootstrap 5"
+        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+      />
+      <Course
+        title="Komple Web"
+        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+      />
+      <Course
+        title="Frontend"
+        description="Deserunt velit elit proident proident. Nostrud Lorem minim ea consequat reprehenderit officia id. Mollit aute nulla in culpa nostrud culpa. Deserunt in anim sunt ea deserunt."
+      />
     </div>
   );
 }
--- src/Course.js
+++ src/Course.js
@@ -4,8 +4,17 @@ import React from "react";
 //   return <div>Course</div>;
 // };
 
-function Course() {
-  return <div>Course</div>;
+function Course({ title, description }) {
+  //   console.log(props);
+  //   const title = props.title;
+  //   const description = props.description;
+  //   const { title, description } = props;
+  return (
+    <div>
+      <div>{title}</div>
+      <div>{description}</div>
+    </div>
+  );
 }
 
 export default Course;
