30f2e08b202e0f2b5250b4477fbd3195801820db
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-07 11:52:38 +0300

    created task create component

--- jiraapplication/src/App.js
+++ jiraapplication/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import TaskCreate from "./components/TaskCreate";
+import TaskList from "./components/TaskList";
 
 function App() {
+  const createTask = (title, taskDesc) => {
+    console.log(title, taskDesc);
+  };
   return (
     <div className="App">
-      Ehlullah
+      <TaskCreate onCreate={createTask} />
+      <h1>Tasks</h1>
+      <TaskList />
     </div>
   );
 }
