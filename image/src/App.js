0118ea526524e22248fda3ddec67aa27716d9f13
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-07 10:01:41 +0300

    created another example project with unsplash api

---
+++ image/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SerachHeader from './SearchHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <SerachHeader/>
+    </div>
+  );
+}
+
+export default App;
---
+++ image/src/SearchHeader.js
@@ -0,0 +1,10 @@
+function SerachHeader() {
+  return <div className="searchDiv">
+    <form>
+        <label>What is your search?</label>
+        <input/>
+    </form>
+  </div>;
+}
+
+export default SerachHeader;
