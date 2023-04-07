89374b3dd3056aca972c496c31d631fbec8c98ea
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-07 12:55:42 +0300

    added update form

--- jiraapplication/src/components/TaskCreate.js
+++ jiraapplication/src/components/TaskCreate.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-function TaskCreate({ onCreate }) {
-  const [title, setTitle] = useState("");
-  const [taskDesc, setTaskDesc] = useState("");
+function TaskCreate({ onCreate, task, taskformUpdate }) {
+  const [title, setTitle] = useState(task ? task.title : '');
+  const [taskDesc, setTaskDesc] = useState(task ? task.taskDesc : '');
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -15,26 +15,56 @@ function TaskCreate({ onCreate }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     onCreate(title, taskDesc);
-    setTitle('');
-    setTaskDesc('');
+    setTitle("");
+    setTaskDesc("");
   };
   return (
-    <div className="task-create">
-      <h3>Please Add Task!</h3>
-      <form className="task-form">
-        <label className="task-label">Title</label>
-        <input value={title} onChange={handleChange} className="task-input" />
-        <label className="task-label">Enter Task!</label>
-        <textarea
-          value={taskDesc}
-          onChange={handleTaskChange}
-          className="task-input"
-          rows={5}
-        />
-        <button className="task-button" onClick={handleSubmit}>
-          Create
-        </button>
-      </form>
+    <div>
+      {taskformUpdate ? (
+        <div className="task-update">
+          <h3>Please Update Task!</h3>
+          <form className="task-form">
+            <label className="task-label">Update Title</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">Update Task!</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button className="task-button update-button" onClick={handleSubmit}>
+              Update
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="task-create">
+          <h3>Please Add Task!</h3>
+          <form className="task-form">
+            <label className="task-label">Title</label>
+            <input
+              value={title}
+              onChange={handleChange}
+              className="task-input"
+            />
+            <label className="task-label">Enter Task!</label>
+            <textarea
+              value={taskDesc}
+              onChange={handleTaskChange}
+              className="task-input"
+              rows={5}
+            />
+            <button className="task-button" onClick={handleSubmit}>
+              Create
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
--- jiraapplication/src/components/TaskShow.js
+++ jiraapplication/src/components/TaskShow.js
@@ -1,20 +1,38 @@
+import { useState } from "react";
+import TaskCreate from "./TaskCreate";
+
 function TaskShow({ task, onDelete }) {
+  const [showEdit, setShowEdit] = useState(false);
+
   const handleDeleteClick = () => {
     onDelete(task.id);
   };
+
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  };
+
   console.log(task);
   return (
     <div className="task-show">
-      <h3 className="task-title">Your Tak</h3>
-      <p>{task.title}</p>
-      <h3 className="task-title">Task List</h3>
-      <p>{task.taskDesc}</p>
-      <div>
-        <button className="task-delete" onClick={handleDeleteClick}>
-          Delete
-        </button>
-        <button className="task-edit">Update</button>
-      </div>
+      {showEdit ? (
+        <TaskCreate task={task} taskformUpdate={true} />
+      ) : (
+        <div>
+          <h3 className="task-title">Your Tak</h3>
+          <p>{task.title}</p>
+          <h3 className="task-title">Task List</h3>
+          <p>{task.taskDesc}</p>
+          <div>
+            <button className="task-delete" onClick={handleDeleteClick}>
+              Delete
+            </button>
+            <button className="task-edit" onClick={handleEditClick}>
+              Update
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
