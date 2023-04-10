4c48467c62c40e12639382f8af4b8f131b2444b3
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-10 16:20:08 +0300

    useTransition example

---
+++ usetransition/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import { useState, useTransition } from "react";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [myList, setMyList] = useState([]);
+  const [isPending, startTransition] = useTransition();
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+
+    startTransition(() => {
+      const myArray = [];
+      for (let i = 0; i < 2000; i++) {
+        myArray.push(e.target.value);
+      }
+      setMyList(myArray);
+    });
+  };
+
+  return (
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+
+      {isPending
+        ? "Loading..."
+        : myList.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
+    </>
+  );
+}
+
+export default App;
