6181cf4b0e38f76aba60bb94c449a08e6ac5984b
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-11 17:13:44 +0300

    added footer

---
+++ burger/src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import '../styles/Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <FacebookIcon />
+        <InstagramIcon />
+        <TwitterIcon />
+      </div>
+      <p>All rights reserved | BurgerEat</p>
+    </div>
+  );
+}
