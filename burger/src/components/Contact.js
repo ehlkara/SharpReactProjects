605f3e4dcfce87af56531a23dd8ea58e20bd2019
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-11 17:54:38 +0300

    edited contact

--- burger/src/components/Contact.js
+++ burger/src/components/Contact.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import BannerImage from "../assets/banner.png";
+import '../styles/Contact.css';
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Contact Us</h1>
+        <form>
+          <label>Name surname</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Please enter name surname..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Please enter email..."
+          />
+          <label>Your Message</label>
+          <textarea
+            rows="6"
+            name="message"
+            placeholder="Please enter message..."
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
 }
