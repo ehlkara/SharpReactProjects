c40afce04c26cf323e6f5d3bb2a4e5b117f0be7f
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-11 17:03:31 +0300

    added navbar

---
+++ burger/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BurgerLogo from "../assets/burgerlogo.png";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="main">
+        <img src={BurgerLogo} alt="" />
+        <div className="mainLink">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
