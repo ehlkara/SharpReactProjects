aead8630640f757d0cc96c86226ef2c2ad3c5e52
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-11 17:40:19 +0300

    menu edited

--- burger/src/components/Menu.js
+++ burger/src/components/Menu.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Data } from "../helpers/Data";
+import MenuItem from "./MenuItem";
+import "../styles/Menu.css";
 
-export default function Menu() {
+export const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="menu">
+      <h1 className="menuTitle">Our Burgers</h1>
+      <div className="menuList">
+        {Data.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
