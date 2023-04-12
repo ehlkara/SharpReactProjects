af7ef9ce6a3159ed0e9b69700bdb2e982f2bf2e6
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-12 10:22:49 +0300

    create general form

---
+++ formproject/src/components/GeneralForm.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { useFormik } from "formik";
+
+export default function GeneralForm() {
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      age: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  //   console.log(formik);
+
+  return (
+    <form>
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+          placeholder="Enter your email..."
+        />
+      </div>
+      <div>
+        <label>Age</label>
+        <input
+          type="number"
+          value={values.age}
+          onChange={handleChange}
+          id="age"
+          placeholder="Enter your age..."
+        />
+      </div>
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          value={values.password}
+          onChange={handleChange}
+          id="password"
+          placeholder="Enter your password..."
+        />
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={values.confirmPassword}
+          onChange={handleChange}
+          id="confirmPassword"
+          placeholder="Enter your confirm password..."
+        />
+      </div>
+      <button type="submit">Save</button>
+    </form>
+  );
+}
