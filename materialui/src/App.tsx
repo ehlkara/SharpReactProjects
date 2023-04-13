12fee20b85fc018b1e1e98449d7696a7fa9b3071
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-13 16:45:20 +0300

    Buttongroup

--- materialui/src/App.tsx
+++ materialui/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { LessonTypography } from './components/LessonTypography';
 import { LessonButton } from './components/LessonButton';
+import { LessonButtonGroup } from './components/LessonButtonGroup';
 
 function App() {
   return (
     <div className="App">
       {/* <LessonTypography/> */}
-      <LessonButton/>
+      {/* <LessonButton/> */}
+      <LessonButtonGroup/>
     </div>
   );
 }
---
+++ materialui/src/components/LessonButtonGroup.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Button, Stack, ButtonGroup } from '@mui/material';
+
+export const LessonButtonGroup = () => {
+    return (
+        <Stack direction='row'>
+            <ButtonGroup variant='contained'>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+            </ButtonGroup>
+            <ButtonGroup variant='outlined'>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+            </ButtonGroup>
+            <ButtonGroup variant='text'>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+            </ButtonGroup>
+            <ButtonGroup variant='text' orientation='vertical'>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+            </ButtonGroup>
+            <ButtonGroup variant='text' orientation='vertical' size='small' color='error'>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+                <Button>My Button</Button>
+            </ButtonGroup>
+        </Stack>
+    )
+}
