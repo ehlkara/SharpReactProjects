a7340cde4219adddbdf92a958114cd8f4654d64f
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-14 10:31:32 +0300

    mulltiple select checkbox

--- materialui/src/components/LessonCheckbox.tsx
+++ materialui/src/components/LessonCheckbox.tsx
@@ -4,7 +4,21 @@ import { Box, FormControlLabel, Checkbox, FormControl, FormLabel, FormGroup, For
 export const LessonCheckbox = () => {
 
     const [isAccept, setIsAccept] = useState(false);
-    console.log(isAccept);
+    const [knowledge, setKnowledge] = useState<string[]>([]);
+    // console.log(isAccept);
+
+
+    const handleKnowledge = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const index = knowledge.indexOf(event.target.value);
+        if (index === -1) {
+            setKnowledge([...knowledge, event.target.value])
+        }
+        else {
+            setKnowledge(knowledge.filter((item) => item !== event.target.value));
+        }
+    }
+
+    console.log(knowledge);
 
     return (
         <Box>
@@ -23,6 +37,16 @@ export const LessonCheckbox = () => {
                     <FormLabel>Education Status</FormLabel>
                     <FormGroup row>
                         <FormControlLabel control={<Checkbox checked={isAccept} onChange={(e) => setIsAccept(e.target.checked)} />} label='I have read and accept the terms of use.' />
+                    </FormGroup>
+                </FormControl>
+            </Box>
+            <Box>
+                <FormControl>
+                    <FormLabel>What do you know in frontend?</FormLabel>
+                    <FormGroup row>
+                        <FormControlLabel value='react' control={<Checkbox checked={knowledge.includes('react')} onChange={handleKnowledge} />} label='React Js' />
+                        <FormControlLabel value='angular' control={<Checkbox checked={knowledge.includes('angular')} onChange={handleKnowledge} />} label='Angular Js' />
+                        <FormControlLabel value='vue' control={<Checkbox checked={knowledge.includes('vue')} onChange={handleKnowledge} />} label='Vue Js' />
                     </FormGroup>
                 </FormControl>
             </Box>
