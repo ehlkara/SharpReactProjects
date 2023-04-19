2ff9d31be275b9111f8a750e84aec2b2a054487e
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-19 10:11:53 +0300

    created form

--- course/src/components/CourseForm.js
+++ course/src/components/CourseForm.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
 export default function CourseForm() {
   return (
-    <div>CourseForm</div>
-  )
+    <div className="courseForm panel">
+      <h4 className="subtitle is-3">Add Course</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input className="input is-expanded" />
+          </div>
+          <div className="field">
+            <label className="label">Description</label>
+            <textarea className="input is-expanded" />
+          </div>
+          <div className="field">
+            <label className="label">Price</label>
+            <input className="input is-expanded" type="number" />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-primary">Save</button>
+        </div>
+      </form>
+    </div>
+  );
 }
