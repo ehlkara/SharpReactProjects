49c00ea2f7202b74a9457837b757a6664a512be8
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-19 11:30:26 +0300

    edited courseValue

--- course/src/components/CourseValue.js
+++ course/src/components/CourseValue.js
@@ -2,18 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 export default function CourseValue() {
-  const totalCost = useSelector(({ form, courses: { data, searchTerm } }) => {
-    return data
-      .filter((course) =>
-        course.name.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-      .reduce((acc, course) => acc + course.cost, 0);
-    // let cost = 0;
-    // for (let course of filteredCourses) {
-    //   cost += course.cost;
-    // }
-    // return cost;
-  });
+  const totalCost = useSelector(
+    ({ form, courses: { data, searchTerm } }) =>
+      data
+        .filter((course) =>
+          course.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .reduce((acc, course) => acc + course.cost, 0)
+  );
 
   return <div className="coursePrice">Toplam Tutar: {totalCost} TL</div>;
 }
