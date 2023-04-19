82c6f07474f0166f728fb6cf41868a46019efac0
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-19 16:01:39 +0300

    created expandablePanel

--- personal/src/components/ExpandablePanel.js
+++ personal/src/components/ExpandablePanel.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 
 export default function ExpandablePanel({ header, children }) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <div className="panelDiv">
       <div className="topArrangement">
         <div className="topArrangement">{header}</div>
-        <div>
-          <GoChevronLeft />
+        <div onClick={handleClick}>
+          {expanded ? (
+            <GoChevronDown style={{ cursor: "pointer" }} />
+          ) : (
+            <GoChevronLeft style={{ cursor: "pointer" }} />
+          )}
         </div>
       </div>
-      {children}
+      {expanded && <div>{children}</div>}
     </div>
   );
 }
