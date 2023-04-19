e638387a93617566e6fe5a10af6121f00231c3aa
ehlullah karakurt <ehlullah.fb@hotmail.com>
2023-04-19 16:40:28 +0300

    created addAlbum and fetchAlbum function

--- personal/src/components/AlbumList.js
+++ personal/src/components/AlbumList.js
@@ -1,8 +1,42 @@
 import React from "react";
-import { useFetchAlbumsQuery } from "../store";
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store";
+import { Button, CircularProgress } from "@mui/material";
+import Skeleton from "@mui/material/Skeleton";
+import AlbumListItem from "./AlbumListItem";
 
 export default function AlbumList({ user }) {
   const { data, isError, isFetching } = useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
 
-  return <div>{user.name} Album</div>;
+  const handleAlbumAdd = () => {
+    addAlbum(user);
+  };
+
+  let content;
+  if (isFetching) {
+    content = (
+      <Skeleton variant="rectangular" sx={{ width: "100%", height: "auto" }} />
+    );
+  } else if (isError) {
+    content = <div>an error has occurred.</div>;
+  } else {
+    content = data.map((album) => {
+      return <AlbumListItem key={album.id} album={album} />;
+    });
+  }
+
+
+  return (
+    <>
+      <div>
+        <div className="topArrangement">
+          <h3>{user.name} Album's</h3>
+          <Button variant="outlined" onClick={handleAlbumAdd}>
+            {results.isLoading ? <CircularProgress /> : <span>Add Album+</span>}
+          </Button>
+        </div>
+      </div>
+      <div>{content}</div>
+    </>
+  );
 }
---
+++ personal/src/components/AlbumListItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AlbumListItem({album}) {
+  return (
+    <div>{album.title}</div>
+  )
+}
