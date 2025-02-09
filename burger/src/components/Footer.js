2f0a7ac07f2b2658d4c5986757d85521dc5b2785
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-02-10 00:36:38 +0300

    customize link

--- burger/src/components/Footer.js
+++ burger/src/components/Footer.js
@@ -2,15 +2,22 @@ import React from "react";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
-import '../styles/Footer.css';
+import "../styles/Footer.css";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <FacebookIcon />
-        <InstagramIcon />
-        <TwitterIcon />
+        <Link to="https://www.facebook.com/">
+          <FacebookIcon />
+        </Link>
+        <Link to="https://www.instagram.com/">
+          <InstagramIcon />
+        </Link>
+        <Link to="https://twitter.com/">
+          <TwitterIcon />
+        </Link>
       </div>
       <p>All rights reserved | BurgerEat</p>
     </div>
