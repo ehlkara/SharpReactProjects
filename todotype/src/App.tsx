9141ac05f82be6a59dcaa33dad8882e43ef2e025
Ehlullah Karakurt <ehlullah.fb@gmail.com>
2025-03-08 00:40:58 +0300

    updated

--- todotype/src/App.tsx
+++ todotype/src/App.tsx
@@ -1,12 +1,11 @@
-import React, { ChangeEvent, FC } from 'react';
-import './App.css';
-import { useState } from 'react';
-import { todoType } from './appTypes';
-import TodoItem from './TodoItem';
+import { ChangeEvent, FC } from "react";
+import "./App.css";
+import { useState } from "react";
+import { todoType } from "./appTypes";
+import TodoItem from "./TodoItem";
 
 const App: FC = () => {
-
-  const [task, setTask] = useState<string>('');
+  const [task, setTask] = useState<string>("");
 
   const [workDay, setWorkDay] = useState<number>(0);
 
@@ -17,35 +16,52 @@ const App: FC = () => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.name === "task") {
       setTask(event.target.value);
-    }
-    else {
+    } else {
       setWorkDay(Number(event.target.value));
     }
   };
 
   const addNewTask = (): void => {
-    const newTask = { taskName: task, workDay: workDay }
+    const newTask = { taskName: task, workDay: workDay };
     setTodoList([...todoList, newTask]);
-    setTask('');
+    setTask("");
     setWorkDay(0);
-  }
+  };
 
   const deleteTask = (nameToDelete: string): void => {
-    setTodoList(todoList.filter((task) => {
-      return task.taskName !== nameToDelete
-    }))
-  }
+    setTodoList(
+      todoList.filter((task) => {
+        return task.taskName !== nameToDelete;
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <div className='maincard'>
-        <input className='maincardinput' type='text' value={task} name='task' placeholder='Enter Your Task...' onChange={handleChange} />
-        <input className='maincardinput' type='number' value={workDay} name='workDay' placeholder='How many days should you complete...' onChange={handleChange} />
-        <button className='maincardbutton' onClick={addNewTask}>New Add Task</button>
+      <div className="maincard">
+        <input
+          className="maincardinput"
+          type="text"
+          value={task}
+          name="task"
+          placeholder="Enter Your Task..."
+          onChange={handleChange}
+        />
+        <input
+          className="maincardinput"
+          type="number"
+          value={workDay}
+          name="workDay"
+          placeholder="How many days should you complete..."
+          onChange={handleChange}
+        />
+        <button className="maincardbutton" onClick={addNewTask}>
+          New Add Task
+        </button>
       </div>
-      <div className='todocard'>
+      <div className="todocard">
         {todoList.map((task: todoType, index: number) => {
-          return <TodoItem key={index} task={task} deleteTask={deleteTask} />
+          return <TodoItem key={index} task={task} deleteTask={deleteTask} />;
         })}
       </div>
     </div>
